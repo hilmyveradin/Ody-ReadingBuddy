277c3fa0f46f1b6df5ab4c506f50c8d0b46c41d5
fattah247 <muhammad.abdul92@ui.ac.id>
2022-04-12 02:50:48 +0700

    TimerBener1

--- Ody-ReadingBuddy/Controller/TimerViewController.swift
+++ Ody-ReadingBuddy/Controller/TimerViewController.swift
@@ -14,43 +14,67 @@ import UIKit
 class TimerViewController: UIViewController {
   
   @IBOutlet weak var timerLabel: UILabel!
-  @IBOutlet weak var startButton: UIButton!
   @IBOutlet weak var pauseButton: UIButton!
   @IBOutlet weak  var resetButton: UIButton!
   @IBOutlet weak var imageView: UIImageView!
   
+  var timer:Timer = Timer()
+  var initialVal:Int = 60
+  var count:Int = 60
+  var isCounting:Bool = false
   
-  var timer = Timer()
-  var seconds = 60
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    let maskotBaca = UIImage.gifImageWithData(<#T##data: Data##Data#>)
-    // Do any additional setup after loading the view.
-  }
-  @IBAction func startButton( sender: Any){
-    
-    timer.invalidate()
-    timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerViewController.timerClass), userInfo: nil, repeats: true)
-  }
-  @IBAction func pauseButton( sender: Any){
-    timer.invalidate()
-    
-  }
-  @IBAction func resetButton( sender: Any){
-    timer.invalidate()
-    seconds = 60
-    timerLabel.text = String(seconds)
-  }
-  
-  @objc func timerClass(){
-    seconds -= 1
-    timerLabel.text = String(seconds)
-    
-    if(seconds == 0){
+  @IBAction func pauseButton(_ sender: Any){
+    if (isCounting){
+      isCounting = false
       timer.invalidate()
+      pauseButton.setImage(UIImage(named: "play.png"), for: .normal)
     }
+    else{
+      isCounting = true
+      pauseButton.setImage(UIImage(named: "pause.png"), for: .normal)
+      timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
+    }
+    
   }
-
+  @objc func timerCounter() -> Void {
+    count = count - 1
+    let time = secMinsHours(seconds: count)
+    let timeString = timeToString(hours: time.0, minutes: time.1, seconds: time.2)
+    timerLabel.text = timeString
+  }
+  
+  func secMinsHours(seconds: Int) -> (Int, Int, Int){
+    return ((seconds / 3600), ((seconds % 36000) / 60), ((seconds % 36000) % 60 ))
+  }
+  
+  func timeToString(hours:Int, minutes:Int, seconds:Int) -> String
+  {
+    var timeString = ""
+    timeString += String(format: "%02d", hours)
+    timeString += " : "
+    timeString += String(format: "%02d", minutes)
+    timeString += " : "
+    timeString += String(format: "%02d", seconds)
+    return timeString
+  }
+  
+  @IBAction func resetPressed(_ sender: Any) {
+    
+    let time2 = secMinsHours(seconds: initialVal)
+    let timeString2 = timeToString(hours: time2.0, minutes: time2.1, seconds: time2.2)
+    let alert = UIAlertController(title: "Reset Timer", message: "Are you sure would like to reset the Timer", preferredStyle: .alert)
+    
+    alert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: { (_) in
+    }))
+    alert.addAction(UIAlertAction(title: "YES", style: .default, handler: { (_) in
+      self.count = self.initialVal
+      self.timer.invalidate()
+      self.timerLabel.text = timeString2
+      self.pauseButton.setImage(UIImage(named: "play.png"), for: .normal)
+    }))
+    
+    self.present(alert, animated: true, completion: nil)
+  }
+  
 }
