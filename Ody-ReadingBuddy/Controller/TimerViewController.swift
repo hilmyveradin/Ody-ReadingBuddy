93ae1129320f9806b67a3a753cd7253da1efcf15
fattah247 <muhammad.abdul92@ui.ac.id>
2022-04-12 11:32:06 +0700

    TimerFix-MainOTW

--- Ody-ReadingBuddy/Controller/TimerViewController.swift
+++ Ody-ReadingBuddy/Controller/TimerViewController.swift
@@ -17,68 +17,134 @@ class TimerViewController: UIViewController {
   @IBOutlet weak  var resetButton: UIButton!
   @IBOutlet weak var imageView: UIImageView!
   
-  var timer:Timer = Timer()
-  var initialVal:Int = 60
-  var count:Int = 60
-  var isCounting:Bool = false
+  var seconds = 60
+  var timer = Timer()
+  var isTimerRunning = true
+  var resumeTapped = false
   
-  override func viewDidLoad() {
-     super.viewDidLoad()
-     
-     let mascotGif = UIImage.gifImageWithName("maskotBaca")
-         imageView.image = mascotGif
-   }
+//  @IBAction func startButtonTapped(_ sender: UIButton){
+//    if isTimerRunning == false {
+//         runTimer()
+//         self.startButton.isEnabled = false
+//    }
+//  }
+  func runTimer() {
+    timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector:(#selector(TimerViewController.updateTimer)), userInfo: nil, repeats: true)
+    isTimerRunning = true
+    pauseButton.isEnabled = true
+  }
   
-  @IBAction func pauseButton(_ sender: Any){
-    if (isCounting){
-      isCounting = false
+  @IBAction func pauseButtonTapped(_ sender: UIButton){
+    if self.resumeTapped == false {
       timer.invalidate()
-      pauseButton.setImage(UIImage(named: "play.png"), for: .normal)
+      self.pauseButton.setImage(UIImage(systemName:"play.fill", compatibleWith: .none), for: .normal)
+      self.resumeTapped = true
+    } else {
+      runTimer()
+      self.pauseButton.setImage(UIImage(systemName:"pause.fill", compatibleWith: .none), for: .normal)
+      self.resumeTapped = false
     }
-    else{
-      isCounting = true
-      pauseButton.setImage(UIImage(named: "pause.png"), for: .normal)
-      timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
+  }
+  
+  @IBAction func resetButtonTapped(_ sender: UIButton){
+    timer.invalidate()
+    seconds = 60    //Here we manually enter the restarting point for the seconds, but it would be wiser to make this a variable or constant.
+    timerLabel.text = timeString(time: TimeInterval(seconds))
+    isTimerRunning = false
+    self.pauseButton.setImage(UIImage(systemName:"play.fill", compatibleWith: .none), for: .normal)
+  }
+  
+  @objc func updateTimer() {
+    if seconds < 1 {
+         timer.invalidate()
+         //Send alert to indicate "time's up!"
+    } else {
+         seconds -= 1
+         timerLabel.text = timeString(time: TimeInterval(seconds))
     }
   }
   
-  @objc func timerCounter() -> Void {
-    count = count - 1
-    let time = secMinsHours(seconds: count)
-    let timeString = timeToString(hours: time.0, minutes: time.1, seconds: time.2)
-    timerLabel.text = timeString
+  func timeString(time:TimeInterval) -> String {
+    let hours = Int(time) / 3600
+    let minutes = Int(time) / 60 % 60
+    let seconds = Int(time) % 60
+    return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
   }
-  
-  func secMinsHours(seconds: Int) -> (Int, Int, Int){
-    return ((seconds / 3600), ((seconds % 36000) / 60), ((seconds % 36000) % 60 ))
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let mascotGif = UIImage.gifImageWithName("maskotBaca")
+          imageView.image = mascotGif
+    runTimer()
+    updateTimer()
   }
-  func timeToString(hours:Int, minutes:Int, seconds:Int) -> String
-  {
-    var timeString = ""
-    timeString += String(format: "%02d", hours)
-    timeString += " : "
-    timeString += String(format: "%02d", minutes)
-    timeString += " : "
-    timeString += String(format: "%02d", seconds)
-    return timeString
-  }
-  
-  @IBAction func resetPressed(_ sender: Any) {
-    
-    let time2 = secMinsHours(seconds: initialVal)
-    let timeString2 = timeToString(hours: time2.0, minutes: time2.1, seconds: time2.2)
-    let alert = UIAlertController(title: "Reset Timer", message: "Are you sure would like to reset the Timer", preferredStyle: .alert)
-    
-    alert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: { (_) in
-    }))
-    alert.addAction(UIAlertAction(title: "YES", style: .default, handler: { (_) in
-      self.count = self.initialVal
-      self.timer.invalidate()
-      self.timerLabel.text = timeString2
-      self.pauseButton.setImage(UIImage(named: "play.png"), for: .normal)
-    }))
-    
-    self.present(alert, animated: true, completion: nil)
-  }
-  
+//  @IBOutlet weak var timerLabel: UILabel!
+//  @IBOutlet weak var pauseButton: UIButton!
+//  @IBOutlet weak  var resetButton: UIButton!
+//  @IBOutlet weak var imageView: UIImageView!
+//
+//  var timer:Timer = Timer()
+//  var initialVal:Int = 60
+//  var count:Int = 60
+//  var isCounting:Bool = false
+//
+//  override func viewDidLoad() {
+//     super.viewDidLoad()
+//
+//     let mascotGif = UIImage.gifImageWithName("maskotBaca")
+//         imageView.image = mascotGif
+//   }
+//
+//  @IBAction func pauseButton(_ sender: Any){
+//    if (isCounting){
+//      isCounting = false
+//      timer.invalidate()
+//      pauseButton.setImage(UIImage(named: "play.png"), for: .normal)
+//    }
+//    else{
+//      isCounting = true
+//      pauseButton.setImage(UIImage(named: "pause.png"), for: .normal)
+//      timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
+//    }
+//  }
+//
+//  @objc func timerCounter() -> Void {
+//    count = count - 1
+//    let time = secMinsHours(seconds: count)
+//    let timeString = timeToString(hours: time.0, minutes: time.1, seconds: time.2)
+//    timerLabel.text = timeString
+//  }
+//
+//  func secMinsHours(seconds: Int) -> (Int, Int, Int){
+//    return ((seconds / 3600), ((seconds % 36000) / 60), ((seconds % 36000) % 60 ))
+//  }
+//  func timeToString(hours:Int, minutes:Int, seconds:Int) -> String
+//  {
+//    var timeString = ""
+//    timeString += String(format: "%02d", hours)
+//    timeString += " : "
+//    timeString += String(format: "%02d", minutes)
+//    timeString += " : "
+//    timeString += String(format: "%02d", seconds)
+//    return timeString
+//  }
+//
+//  @IBAction func resetPressed(_ sender: Any) {
+//
+//    let time2 = secMinsHours(seconds: initialVal)
+//    let timeString2 = timeToString(hours: time2.0, minutes: time2.1, seconds: time2.2)
+//    let alert = UIAlertController(title: "Reset Timer", message: "Are you sure would like to reset the Timer", preferredStyle: .alert)
+//
+//    alert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: { (_) in
+//    }))
+//    alert.addAction(UIAlertAction(title: "YES", style: .default, handler: { (_) in
+//      self.count = self.initialVal
+//      self.timer.invalidate()
+//      self.timerLabel.text = timeString2
+//      self.pauseButton.setImage(UIImage(named: "play.png"), for: .normal)
+//    }))
+//
+//    self.present(alert, animated: true, completion: nil)
+//  }
+//
 }
--- Ody-ReadingBuddy/Controller/mainViewController.swift
+++ Ody-ReadingBuddy/Controller/mainViewController.swift
@@ -11,53 +11,185 @@ import UIKit
 
 class mainViewController: UIViewController {
   
-  @IBOutlet weak var mainImageView: UIImageView!
   
-  let shapeLayer = CAShapeLayer()
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var startBtnUI: UIButton!
+  
+  @IBOutlet weak var daySpentUI: UILabel!
+  @IBOutlet weak var timeSpentUI: UILabel!
   
   override func viewDidLoad() {
-     super.viewDidLoad()
-     let maskotGif = UIImage.gifImageWithName("mascotfin")
-         mainImageView.image = maskotGif
+    super.viewDidLoad()
+      
+    //otter gif
+    let mascotGif = UIImage.gifImageWithName("masccotfin")
+    imageView.image = mascotGif
+     
+    //progress bar
+    timeSpent()
+    daysSpent()
     
-   
-    let center = view.center
+  }
+  
+  @IBAction func startReadingClicked(_ sender: UIButton) {
+    //check goal setting
+    //no goal
+    let alert = UIAlertController(title: "You haven't set any goal", message: "Set goal before you start. Go to 'Goals'?", preferredStyle: .alert)
+
+    alert.addAction(UIAlertAction(title: "Yes", style: .cancel, handler: { action in
+      //open goals
+      
+      }))
     
-    // track layer
+    alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+
+    self.present(alert, animated: true)
+  }
+  
+  func daysSpent(){
+    //draw circle
+    let position = CGPoint(x: 135, y: 200)
+    
+    //create track layer
     let trackLayer = CAShapeLayer()
-    let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi/2, endAngle: 2*CGFloat.pi, clockwise: true)
+    let circularPath = UIBezierPath(arcCenter: position, radius: 45, startAngle: -CGFloat.pi/2, endAngle: 2 * CGFloat.pi, clockwise: true)
     trackLayer.path = circularPath.cgPath
-    trackLayer.strokeColor = UIColor.lightGray.cgColor
-    trackLayer.lineWidth = 10
+    
+    trackLayer.strokeColor = UIColor.systemGray5.cgColor
+    trackLayer.lineWidth = 8
     trackLayer.fillColor = UIColor.clear.cgColor
     trackLayer.lineCap = CAShapeLayerLineCap.round
-    
     view.layer.addSublayer(trackLayer)
     
-    //
+    //create progress layer
+    let shapeLayer = CAShapeLayer()
     
-   // let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi/2, endAngle: 2*CGFloat.pi, clockwise: true)
     shapeLayer.path = circularPath.cgPath
-    shapeLayer.strokeColor = UIColor.orange.cgColor
-    shapeLayer.lineWidth = 10
-    shapeLayer.fillColor = UIColor.clear.cgColor
     
+    shapeLayer.strokeColor = UIColor.orange.cgColor
+    shapeLayer.lineWidth = 8
+    shapeLayer.fillColor = UIColor.clear.cgColor
     shapeLayer.lineCap = CAShapeLayerLineCap.round
     
     shapeLayer.strokeEnd = 0
-    
+  
     view.layer.addSublayer(shapeLayer)
-    view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
-   }
-
-  @objc private func handleTap(){
     
+    //create reading progress animation
     let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-    basicAnimation.toValue = 1
+    
+    //get data
+    var realization = 25
+    var target = 50
+    var progress: Double = Double(1) / Double(target) * Double(realization)
+    
+    basicAnimation.toValue = progress
     basicAnimation.duration = 1
     basicAnimation.fillMode = CAMediaTimingFillMode.forwards
     basicAnimation.isRemovedOnCompletion = false
     
-    shapeLayer.add(basicAnimation, forKey:"urSoBasic")
+    shapeLayer.add(basicAnimation, forKey: "animation")
+    
+    //set label
+    daySpentUI.text = "\(realization)/\(target)"
   }
+  
+  func timeSpent(){
+    //draw circle
+    let position = CGPoint(x: 290, y: 200)
+    
+    //create track layer
+    let trackLayer = CAShapeLayer()
+    let circularPath = UIBezierPath(arcCenter: position, radius: 45, startAngle: -CGFloat.pi/2, endAngle: 2 * CGFloat.pi, clockwise: true)
+    trackLayer.path = circularPath.cgPath
+    
+    trackLayer.strokeColor = UIColor.systemGray5.cgColor
+    trackLayer.lineWidth = 8
+    trackLayer.fillColor = UIColor.clear.cgColor
+    trackLayer.lineCap = CAShapeLayerLineCap.round
+    view.layer.addSublayer(trackLayer)
+    
+    //create progress layer
+    let shapeLayer = CAShapeLayer()
+    
+    shapeLayer.path = circularPath.cgPath
+    
+    shapeLayer.strokeColor = UIColor.orange.cgColor
+    shapeLayer.lineWidth = 8
+    shapeLayer.fillColor = UIColor.clear.cgColor
+    shapeLayer.lineCap = CAShapeLayerLineCap.round
+    
+    shapeLayer.strokeEnd = 0
+  
+    view.layer.addSublayer(shapeLayer)
+    
+    //create reading progress animation
+    
+    let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+    
+    //get data
+    var realization = 10
+    var target = 15
+    var progress: Double = Double(1) / Double(target) * Double(realization)
+    
+    basicAnimation.toValue = progress
+    basicAnimation.duration = 1
+    basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+    basicAnimation.isRemovedOnCompletion = false
+    
+    shapeLayer.add(basicAnimation, forKey: "animation")
+    
+    //set label
+    timeSpentUI.text = "\(realization)m"
+  }
+  
+//  @IBOutlet weak var mainImageView: UIImageView!
+//
+//  let shapeLayer = CAShapeLayer()
+//
+//  override func viewDidLoad() {
+//     super.viewDidLoad()
+//     let maskotGif = UIImage.gifImageWithName("mascotfin")
+//         mainImageView.image = maskotGif
+//
+//
+//    let center = view.center
+//
+//    // track layer
+//    let trackLayer = CAShapeLayer()
+//    let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi/2, endAngle: 2*CGFloat.pi, clockwise: true)
+//    trackLayer.path = circularPath.cgPath
+//    trackLayer.strokeColor = UIColor.lightGray.cgColor
+//    trackLayer.lineWidth = 10
+//    trackLayer.fillColor = UIColor.clear.cgColor
+//    trackLayer.lineCap = CAShapeLayerLineCap.round
+//
+//    view.layer.addSublayer(trackLayer)
+//
+//    //
+//
+//   // let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi/2, endAngle: 2*CGFloat.pi, clockwise: true)
+//    shapeLayer.path = circularPath.cgPath
+//    shapeLayer.strokeColor = UIColor.orange.cgColor
+//    shapeLayer.lineWidth = 10
+//    shapeLayer.fillColor = UIColor.clear.cgColor
+//
+//    shapeLayer.lineCap = CAShapeLayerLineCap.round
+//
+//    shapeLayer.strokeEnd = 0
+//
+//    view.layer.addSublayer(shapeLayer)
+//    view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+//   }
+//
+//  @objc private func handleTap(){
+//
+//    let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+//    basicAnimation.toValue = 1
+//    basicAnimation.duration = 1
+//    basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+//    basicAnimation.isRemovedOnCompletion = false
+//
+//    shapeLayer.add(basicAnimation, forKey:"urSoBasic")
+//  }
 }
